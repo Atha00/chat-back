6889ee184f975de15e60c44d3ccf1281a2d19741
Michelot Alexis <amichelot@club-internet.fr>
2021-03-03 18:06:28 +0100

    first step for list of connected users

--- index.js
+++ index.js
@@ -9,24 +9,48 @@ const io = require("socket.io")(http, {
 
 const getRandomFruitsName = require("random-fruits-name");
 
-// app.get("/", (req, res) => {
-//   res.sendFile("/Users/milok/Teacher-Assist/Session21-03/socket-io/index.html");
-// });
 // io.emit("connection", {
 //   someProperty: "some value",
 //   otherProperty: "other value"
 // });
 io.on("connection", socket => {
-  //   socket.broadcast.emit("hi");
-  let name = getRandomFruitsName("fr");
-  socket.broadcast.emit("chat message", name + " is connected");
-  socket.broadcast.emit("connection", name + ": Hi there !");
+  let users = [];
+  const { name } = socket.handshake.query;
+  socket.nickname = name;
+  socket.join("room1");
+  const clients = socket.adapter.rooms.get("room1");
+  // console.log("clients =>", clients);
+  for (const clientId of clients) {
+    // console.log("verif =>", clientId);
+    //this is the socket of each client in the room.
+    const clientSocket = io.sockets.sockets.get(clientId);
+    users.push(clientSocket.nickname);
+  }
+  // console.log(users);
+  socket.on("user list", () => {
+    io.emit("user list", users);
+  });
+  // console.log(socket.rooms);
+  socket.broadcast.emit("connection", name + " is connected");
+  // socket.on("name select", name => {
+  //     io.emit("name select", name)
+  //   })
   socket.on("disconnect", () => {
+    console.log(users);
+    let index;
+    for (let i = 0; i < users.length; i++) {
+      if (users[i] === name) {
+        index = i;
+      }
+    }
+    users.splice(index, 1);
+    console.log(users);
     io.emit("chat message", name + " disconnect");
   });
   socket.on("chat message", msg => {
     io.emit("chat message", name + ": " + msg);
   });
+
   socket.on("is typing", text => {
     socket.broadcast.emit("is typing", name + " is typing...");
   });
